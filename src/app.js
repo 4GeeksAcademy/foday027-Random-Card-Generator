ccba3fd998e2b911e2832cc8cec426da5a64fad3
Foday027 <159056257+Foday027@users.noreply.github.com>
2024-05-12 16:39:49 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const suit = ["♥", "♠", "♣", "♦"];
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function randomCard() {
+    let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+    let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+    document.querySelector(".card-symbol1").innerHTML = randomSuit; // Use innerHTML to set HTML content
+    document.querySelector(".number").innerHTML = randomNumber;
+    document.querySelector(".card-symbol2").innerHTML = randomSuit;
+
+    if (randomSuit === "♥" || randomSuit === "♦") {
+      document.querySelector(".card-symbol1").style.color = "red";
+      document.querySelector(".card-symbol2").style.color = "red";
+    } else if (randomSuit === "♠" || randomSuit === "♣") {
+      document.querySelector(".card-symbol1").style.color = "black";
+      document.querySelector(".card-symbol2").style.color = "black";
+    }
+  }
+  randomCard();
 };
